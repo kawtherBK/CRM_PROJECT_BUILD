0310068547e922f602383624bfdd29b43ec3618e
kawther boukamcha <boukamchakawther@gmail.com>
2022-08-25 15:15:48 +0100

    new build

---
+++ build/precache-manifest.f7aace7e21c2ceb18e0a62a06eb2ffd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d1636dd674b9663d508",
+    "url": "/static/css/main.5d42b531.chunk.css"
+  },
+  {
+    "revision": "5d1636dd674b9663d508",
+    "url": "/static/js/main.5d1636dd.chunk.js"
+  },
+  {
+    "revision": "6d1c687f73f1a0446a07",
+    "url": "/static/js/1.6d1c687f.chunk.js"
+  },
+  {
+    "revision": "f6349e0f94a769ef41a9",
+    "url": "/static/css/2.f95967ac.chunk.css"
+  },
+  {
+    "revision": "f6349e0f94a769ef41a9",
+    "url": "/static/js/2.f6349e0f.chunk.js"
+  },
+  {
+    "revision": "9d519b82a488b3016fee",
+    "url": "/static/js/runtime~main.9d519b82.js"
+  },
+  {
+    "revision": "9c083ebd6d94e047731e641c81f24602",
+    "url": "/index.html"
+  }
+];
